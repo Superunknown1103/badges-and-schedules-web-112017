c0a14b179dae51ca0dada377796f2a1d840354c9
Bruce Tsvilikhovsky <bruce.tsvilikhovsky@flatironschool.com>
2019-03-22 18:59:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}"
+end
+
+def batch_badge_creator(speakers)
+  badges = []
+  speakers.each do | i |
+    badges.push("Hello, my name is #{i}.")
+  end
+  return badges;
+end
+
+def assign_rooms(speakers)
+  messages = []
+  speakers.each_with_index { | i, index |
+  messages.push("Hello, #{i}! You'll be assigned to room #{index + 1}!")
+  }
+  return messages
+end
+
+def badge_maker()
